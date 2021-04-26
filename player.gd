e9fd4b96609991fbdb368e7e6f8e98b05a79047d
Lucas Brill Lopes <36541997+lokingin@users.noreply.github.com>
2021-04-26 13:32:00 -0300

    Accel and Deaccel update
    +Made a condition on input so it matches the deaccelleration when swapping controls.
    +Added ACCEL and DEACCEL variables.
    
    -removed some empty unnecessary lines

--- player.gd
+++ player.gd
@@ -4,11 +4,13 @@
 #---------------------------------
 #Cachal (Lucas Brill Lopes) - 26/04/2021
 #
-
 extends KinematicBody2D
 
 export var VEL = 75
 export var MAX_VEL = 125
+
+export var ACCEL = 1.0
+export var DEACCEL = 2.5
 
 var MAX_FLOOR_ANGLE = 45.0
 
@@ -19,31 +21,39 @@ var velocity = Vector2()
 
 onready var ground_detector = get_node("ground_detector")
 
+#For now I don't need it.
 func _ready():
-	
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	#cheks if the keys are pressed
 	if !Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
 		#these give a smooth transition when releasing the button
 		if velocity.x > 0:
-			velocity.x -= 2.5
+			velocity.x -= DEACCEL
 		if velocity.x < 0:
-			velocity.x += 2.5
+			velocity.x += DEACCEL
 	
 	#the usual map input config with the direction values
 	#giving a simple velocity condition to halt the player so he stays near the
 	#maximum velocity
 	if Input.is_action_pressed("left") and velocity.x > -MAX_VEL:
-		velocity.x -= 1.0
+		#if velocity.x is 0 or lower, keep ACCEL
+		if velocity.x <= 0:
+			velocity.x -= ACCEL
+		#if velocity.x is higher then use DEACCEL to help deaccellerate
+		#this fix the changing direction deaccel problem
+		if velocity.x > 0:
+			velocity.x -= DEACCEL
 	if Input.is_action_pressed("right") and velocity.x < MAX_VEL:
-		velocity.x += 1.0
+		if velocity.x >= 0:
+			velocity.x += ACCEL
+		if velocity.x < 0:
+			velocity.x += DEACCEL
 	
 	#normalize and multiplies by VEL var.
 	velocity.normalized().x = velocity.x * VEL
 
-	
 	#the jump action, always make sure you have a raycast to detect ground
 	#collision.
 	if ground_detector.is_colliding():
